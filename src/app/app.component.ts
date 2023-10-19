df9dfeea901d330760110790098dc208a28c4f8c
Wataru Sato <awwa@users.noreply.github.com>
2023-10-19 23:03:51 +0900

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
+  template: `<h1>Hello world!</h1>`,
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mincuru-angular';
+  title = 'homes';
 }
