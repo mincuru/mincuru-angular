21b8bcce1ab819403a59a1a5a13bfa533f38288e
Wataru Sato <awwa@users.noreply.github.com>
2023-10-27 00:48:01 +0900

    layout整理

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    RouterModule,
-    HeaderComponent,
-    MatSidenavModule,
-  ],
+  imports: [CommonModule, RouterOutlet, RouterModule, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'mincuru-angular';
--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-cars',
   standalone: true,
-  imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule, MatButtonModule, MatSidenavModule, MatListModule],
   templateUrl: './cars.component.html',
-  styleUrls: ['./cars.component.sass'],
+  styleUrls: ['./cars.component.scss'],
 })
 export class CarsComponent {}
